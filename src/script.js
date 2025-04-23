894089dbabacd5c6f5cf5f0ddd711a3e28d7f07e
fredrik.r.lundin@gmail.com <fredrik.r.lundin@gmail.com>
2025-04-23 21:43:53 +0200

    First commit

--- src/script.js
+++ src/script.js
@@ -1,19 +1,41 @@
 // Definera array av namn
-let people;
+let people = ["Anna", "Simon", "Lars", "Agneta"]
 
 
 export function initSearch(people) { // rör ej denna rad
 
   // Hämta HTML-element
+  let button = document.getElementById("searchBtn")
+  let input = document.getElementById("searchInput")
+  let htmlText = document.getElementById("resultDisplay")
   
   // Lägg till en klickhändelse på "Sök"-knappen
-  searchBtn.addEventListener("click",() => {
+  button.addEventListener("click",() => {
   
     // 1. Hämta texten i searchInput.value
+
+    let userInput = input.value
     
     // 2. Skapa en boolean för found
+
+    let nameFound = false
     
     // 3. Loopa igenom people-arrayen
+
+    for (const name of people)
+    {
+      if (name === userInput)
+      {
+        htmlText.innerText = "Namn hittades!"
+        nameFound = true
+      }
+    }
+
+    if (!nameFound)
+    {
+      htmlText.innerText = "Namn hittades inte."
+    }
+
     
     // 4. Jämför texten med varje namn i arrayen
     
